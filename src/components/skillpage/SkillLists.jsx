2816bfd3a2a49e10f061d5a1e5052dac95365f25
xrayenjn <jordannathanael0@gmail.com>
2025-02-12 20:06:02 +1100

    feat: Fill the Details for the skillPage

--- src/components/skillpage/SkillLists.jsx
+++ src/components/skillpage/SkillLists.jsx
@@ -1,6 +1,96 @@
 const SkillLists = () => {
   return (
-    <div>SkillLists</div>
+    <section className="flex justify-around p-15 space-x-20 m:flex-row ssm:flex-col space-y-10">
+      <div className="w-1/2 ssm:w-fit">
+        <div className="py-3">
+          <div className="border-2 border-solid border-white-800">
+            {/* Preferred Languages */}
+            <h1 className="text-xl text-slate-100 px-1">Preferred Languages</h1>
+            <ul className="list-disc px-6">
+              <li>Java</li>
+              <li>Javascript</li>
+              <li>Swift</li>
+              <li>Python</li>
+            </ul>
+          </div>
+        </div>
+        <div className="py-3">
+          <div className="border-2 border-solid border-white-800">        
+            {/* Other known languages */}
+            <h1 className="text-xl text-slate-100 px-1">Other Known Languages</h1>
+            <ul className="list-disc px-6">
+              <li>C++</li>
+              <li>C</li>
+              <li>C#</li>
+              <li>Typescript</li>
+              <li>Kotlin</li>
+              <li>Bash and Shell Script</li>
+            </ul>
+          </div>
+        </div>
+        <div className="py-3">
+          <div className="border-2 border-solid border-white-800">        
+            {/* Professional Experiences */}
+            <h1 className="text-xl text-slate-100 px-1">Professional Experiences</h1>
+            <ul className="list-disc px-6">
+              <li>Newly Appointed Assistant Lecturer</li>
+              <li>Almost 2 years of Teaching Associate Experience</li>
+              <li>Full Stack Web Development</li>
+            </ul>
+          </div>
+        </div>
+        <div className="py-3">
+          <div className="border-2 border-solid border-white-800">        
+            {/* Other Skills */}
+            <h1 className="text-xl text-slate-100 px-1">Other Skills</h1>
+            <ul className="list-disc px-6">
+              <li>3D Modeling</li>
+              <li>Pixel Art</li>
+              <li>Teaching</li>
+              <li>Music (Organ, Trumpet)</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 ssm:w-fit">
+        <div className="py-3">
+          <div className="border-2 border-solid border-white-800">        
+            {/* Technologies */}
+            <h1 className="text-xl text-slate-100 px-1">Technologies</h1>
+            <ul className="list-disc px-6">
+              <li>WebDev:</li>
+              <li>Databases:</li>
+              <li>Mobile App Development:</li>
+              <li>Source Control:</li>
+              <li>Gamedev:</li>
+            </ul>
+          </div>
+        </div>
+        <div className="py-3">
+          <div className="border-2 border-solid border-white-800">        
+            {/* Achievements */}
+            <h1 className="text-xl text-slate-100 px-1">Achievements</h1>
+            <ul className="list-disc px-6">
+              <li>Awarded for highest grade achiever for a third level unit in University</li>
+              <li>Awarded for highest average mark for diploma student for that batch</li>
+              <li>Awarded for highest grade achievers for 5 units in College</li>
+              <li>Awarded for an exceptional work ethic</li>
+            </ul>
+          </div>
+        </div>
+        <div className="py-3">
+          <div className="border-2 border-solid border-white-800">        
+            {/* Education */}
+            <h1 className="text-xl text-slate-100 px-1">Education</h1>
+            <ul className="list-disc px-6">
+              <li>Bachelor of Computer Science (3.625 GPA - 83.29 WAM)</li>
+              <li>Diploma of Information Technology (92.1 WAM)</li>
+              <li>Self Taught</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/SkillPage.jsx
+++ src/pages/SkillPage.jsx
@@ -1,6 +1,13 @@
+import Footer from "../components/Footer"
+import { Header } from "../components/mainpage/Header"
+import SkillLists from "../components/skillpage/SkillLists"
+
 const SkillPage = () => {
   return (
-    <div>SkillPage</div>
+    <div className="bg-blue-300">
+      <SkillLists />
+      <Footer />
+    </div>
   )
 }
 
