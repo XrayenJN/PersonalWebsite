2e71baed0e2b776ff6f02b047cfb7a53d792722b
xrayenjn <jordannathanael0@gmail.com>
2025-02-12 15:55:29 +1100

    feat: Initial Code of React-Vite and TailwindCss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='container max-w-screen-xl mx-auto px-4'>
+        <h1>Hello</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
