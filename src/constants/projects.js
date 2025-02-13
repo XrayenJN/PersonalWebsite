975c76bd091be64f6426e67a1dc8849bfaffae30
xrayenjn <jordannathanael0@gmail.com>
2025-02-13 18:53:47 +1100

    feat: add the project details contant

---
+++ src/constants/projects.js
@@ -0,0 +1,30 @@
+import { RxArchive } from "react-icons/rx";
+
+import backgroundCard from "../assets/card_backround.jpg"
+
+export const ProjectsData = [
+  {
+    icon: RxArchive,
+    title: "Team Management System",
+    content: "React, Express, Firebase",
+    backgroundImage: backgroundCard,
+  },
+  {
+    icon: RxArchive,
+    title: "Document Reminder",
+    content: "React, Firebase",
+    backgroundImage: backgroundCard,
+  },
+  {
+    icon: RxArchive,
+    title: "MMO Games Library",
+    content: "React, Firebase",
+    backgroundImage: backgroundCard,
+  },
+  {
+    icon: RxArchive,
+    title: "Nine Man Morris Game",
+    content: "Java, Swing",
+    backgroundImage: backgroundCard,
+  },
+]
