79071666f39de7ee9e84a36c913f903cd7c210b0
xrayenjn <jordannathanael0@gmail.com>
2025-02-14 17:41:29 +1100

    feat: add dark blue background and blurness to it for SkillIntro section

--- src/components/mainpage/SkillIntro.jsx
+++ src/components/mainpage/SkillIntro.jsx
@@ -1,13 +1,22 @@
+import DarkBlueBackground from '../../assets/blue-dark-background.jpg'
+
 const SkillIntro = () => {
   return (
-    <div className="flex justify-around items-center p-8 space-x-10 m:flex-row ssm:flex-col space-y-2">
-      <div>
-        <h1 className="text-center text-2xl">Assistant Lecturer</h1>
-        <p className="text-center">I'm an assistant lecturer by trade, but I do enjoy programming!</p>
-        <hr/>
-        <a href="/skills">
-          <p className="text-center py-5 text-blue-600 font-bold">Check my Skillset</p>
-        </a>
+    <div 
+      className='bg-cover border-t-2'
+      style={{backgroundImage: `url(${DarkBlueBackground})`}}
+      >
+      <div 
+        className="flex justify-around items-center p-8 space-x-10 m:flex-row ssm:flex-col space-y-2 backdrop-blur-md"
+        >
+        <div>
+          <h1 className="text-center text-2xl">Assistant Lecturer</h1>
+          <p className="text-center">I'm an assistant lecturer by trade, but I do enjoy programming!</p>
+          <hr/>
+          <a href="/skills">
+            <p className="text-center py-5 text-red-600 font-bold">Check my Skillset</p>
+          </a>
+        </div>
       </div>
     </div>
   )
