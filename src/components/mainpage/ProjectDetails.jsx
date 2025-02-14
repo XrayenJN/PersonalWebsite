a3022751f2b6324efd38c40cc0ad1be495eee348
xrayenjn <jordannathanael0@gmail.com>
2025-02-14 17:01:19 +1100

    fix: remove width option to make the area neater

--- src/components/mainpage/ProjectDetails.jsx
+++ src/components/mainpage/ProjectDetails.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
 const ProjectDetails = ({project}) => {
-
-
   return (
     <div className='p-10 flex items-center gap-10'>
-      <div className="flex flex-col gap-10 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[300px] w-[200px] lg:h-[500px] lg:w-[500px] overflow-hidden cursor-pointer">
+      <div className="flex flex-col gap-10 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[300px] w-[200px] lg:h-[500px] lg:w-[500px] overflow-hidden cursor-pointer">
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{ backgroundImage: `url(${project.backgroundImage})` }}
