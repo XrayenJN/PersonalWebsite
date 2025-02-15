d01e6355f00d7743be589f12e6db90a8b4f6b194
xrayenjn <jordannathanael0@gmail.com>
2025-02-15 22:11:56 +1100

    feat: add my own pixelart logo for github linkedin cv email icon

--- src/components/mainpage/Hero.jsx
+++ src/components/mainpage/Hero.jsx
@@ -2,6 +2,10 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import HeroPic from '../../assets/hyouka-icon.png';
 import { MdEmail } from 'react-icons/md';
 import { DiGoogleAnalytics } from 'react-icons/di';
+import GithubIcon from "../../assets/github.png";
+import LinkedinIcon from "../../assets/linkedin.png";
+import EmailIcon from "../../assets/email.png";
+import CvIcon from "../../assets/cv.png";
 
 const Hero = () => {
   return (
@@ -11,10 +15,16 @@ const Hero = () => {
         <hr/>
         <p className="mt-4 text-md text-slate-100 ">Junior Software Developer, Assistant Lecturer, and Computer Science Graduate</p>
         <div className='flex mt-5 space-x-4 cursor-pointer'>
-          <a href='https://github.com/XrayenJn' target="_blank"><BsGithub size={35} className='border-3 hover:border-blue-600 rounded-full' /></a>
-          <a href='https://au.linkedin.com/in/jordan-nathanael' target="_blank"><BsLinkedin size={35} className='border-3 hover:border-blue-600 rounded-full' /></a>
-          <a href='mailto:jordannathanael0@gmail.com' target="_blank"><MdEmail size={35} className='border-3 hover:border-blue-600 rounded-full' /></a>
-          <a href='https://drive.google.com/file/d/12SjD0fpeww_Ul1K5a5Dj4p0rhvDqsv3x/view?usp=drive_link' target="_blank"><DiGoogleAnalytics size={35} className='border-3 hover:border-blue-600 rounded-full' /></a>          
+          <a href='https://github.com/XrayenJn' target="_blank">
+            <img src={GithubIcon} alt="" width={35} height={35}
+            className='border-1 hover:border-blue-600 rounded-full' />
+          </a>
+          <a href='https://au.linkedin.com/in/jordan-nathanael' target="_blank"><img src={LinkedinIcon} alt="" width={35} height={35}
+            className='border-1 hover:border-blue-600 rounded-full' /></a>
+          <a href='mailto:jordannathanael0@gmail.com' target="_blank"><img src={EmailIcon} alt="" width={35} height={35}
+            className='border-1 hover:border-blue-600 rounded-full' /></a>
+          <a href='https://drive.google.com/file/d/12SjD0fpeww_Ul1K5a5Dj4p0rhvDqsv3x/view?usp=drive_link' target="_blank"><img src={CvIcon} alt="" width={35} height={35}
+            className='border-1 hover:border-blue-600 rounded-full' /></a>          
         </div>
       </div>
       <div className='w-1/6 ssm:w-fit'>
