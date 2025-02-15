3abe8d6c6d4ff3a84d31069ae595d3dddfc7a4b9
xrayenjn <jordannathanael0@gmail.com>
2025-02-15 22:12:26 +1100

    fix: change the colour transition bcs it doesn't work well in force-darkmode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { MdEmail } from "react-icons/md"
 
 const Footer = () => {
   return (
-    <div className="border-t-2 flex justify-around p-5 space-x-10 m:flex-row ssm:flex-col space-y-2 bg-radial from-white to-black">
+    <div className="border-t-2 flex justify-around p-5 space-x-10 m:flex-row ssm:flex-col space-y-2 bg-radial from-slate-900 to-black">
       <div className="items-center">
           <h1 className="text-center text-2xl">Contact</h1> 
           <p className="text-center">Please contact me for more details at 
