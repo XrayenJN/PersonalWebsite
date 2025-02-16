fa9a7bb5231dbeb7295d4504584c649fe4d2028d
xrayenjn <jordannathanael0@gmail.com>
2025-02-16 20:04:27 +1100

    feat: add description for each project

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,36 +1,40 @@
 import { RxArchive } from "react-icons/rx";
 
 import WarehouseIcon from "../assets/warehouse.png";
+import TeamManagementIcon from "../assets/teammanagement.png";
+import DocReminderIcon from "../assets/docreminder.png";
+import BookPileIcon from "../assets/bookpile.png";
+import GameIcon from "../assets/discord.png";
 
 export const ProjectsData = [
   {
     icon: WarehouseIcon,
     title: "Warehourse System",
     content: "Kotlin",
-    description: ""
+    description: "Work-In-Progress."
   },
   {
-    icon: RxArchive,
+    icon: TeamManagementIcon,
     title: "Team Management System",
     content: "React, Express, Firebase",
-    description: ""
+    description: "In short, it is like Github Project / Jira Replica. However, it provides 4 kind of view such as kanban board, calendar, list, and timeline view."
   },
   {
-    icon: RxArchive,
+    icon: DocReminderIcon,
     title: "Document Reminder",
     content: "React, Firebase",
-    description: ""
+    description: "The system provides a reminder to the client about a particular listing. Any notification will be sent through email."
   },
   {
-    icon: RxArchive,
+    icon: BookPileIcon,
     title: "MMO Games Library",
-    content: "Swift",
-    description: ""
+    content: "Swift, Firebase",
+    description: "iOS mobile application by using core data and external API from mmobomb.com itself."
   },
   {
-    icon: RxArchive,
+    icon: GameIcon,
     title: "Nine Man Morris Game",
     content: "Java, Swing",
-    description: ""
+    description: "Nine Man Morris game by prioritizing the engine architecture than the UI component."
   },
 ]
