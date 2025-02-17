2a7d3d8f3cf8bb176f0aaaa2e408b5a6e2d987b3
xrayenjn <jordannathanael0@gmail.com>
2025-02-17 20:56:49 +1100

    fix: separate the <p> into two ones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,10 @@ import { MdEmail } from "react-icons/md"
 const Footer = () => {
   return (
     <div className="border-t-2 flex justify-around p-5 space-x-10 m:flex-row ssm:flex-col space-y-2 bg-radial from-slate-900 to-black">
-      <div className="items-center">
+      <div className="justify-items-center">
           <h1 className="text-center text-2xl">Contact</h1> 
-          <p className="text-center">Please contact me for more details at 
+          <p className="text-center">Please contact me for more details at </p> 
           <p className="inline text-red-900 font-bold"> jordannathanael0@gmail.com</p>
-        </p>
       </div>
     </div>
   )
